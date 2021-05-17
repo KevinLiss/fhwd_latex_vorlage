a14ff9ab214c14b9d19c75b95595980841ea46b3
Kevin Liss <kevin.liss@hotmail.de>
2021-05-17 16:40:05 +0200

    Init

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# Vorlage einer Wissenschaftlichen Arbeit für die FHDW Paderborn in LaTeX
+
+- installieren sie LaTeX auf ihrem Computer
+    - https://www.latex-project.org/get/
+- Defeniere die Variablen in der Thesis.tex
+- Passe deine Chapter ein
+- Nutze die Bibliothek unter /library
+- Baue die PDF (in Windows unter der Ubuntu WSL) mit dem Skript ./build.sh
---
+++ build.sh
@@ -0,0 +1,48 @@
+pdflatex ./Thesis -draftmode
+biber ./Thesis
+makeindex -s ./Thesis.ist -t ./Thesis.alg -o ./Thesis.acr ./Thesis.acn
+makeglossaries ./Thesis
+pdflatex ./Thesis -draftmode
+pdflatex ./Thesis
+
+zip -r ./Quellen.zip ./quellen/*
+
+# Auto Git-Commit
+git add .
+git commit -am "$(date +"%D %T")"
+
+# Remove unassasary files
+# ==all Thesis files
+rm ./Thesis.acr
+rm ./Thesis.acn
+rm ./Thesis.alg
+rm ./Thesis.aux
+rm ./Thesis.bcf
+rm ./Thesis.fdb_latexmk
+rm ./Thesis.fk
+rm ./Thesis.fls
+rm ./Thesis.ist
+rm ./Thesis.lof
+rm ./Thesis.log
+rm ./Thesis.lol
+rm ./Thesis.lot
+rm ./Thesis.maf
+rm ./Thesis.mtc
+rm ./Thesis.mtc0
+rm ./Thesis.out
+rm ./Thesis.synctex.gz
+rm ./Thesis.toc
+rm ./Thesis.bbl
+rm ./Thesis.bbl
+rm ./Thesis.blg
+# ==all.aux Files
+rm ./chapter/Anhang.aux
+rm ./chapter/Ehrenwoertliche_Erklaerung.aux
+rm ./chapter/Einleitung.aux
+rm ./chapter/Grundlagen.aux
+rm ./chapter/Praxisteil.aux
+rm ./chapter/Quellenverzeichnis.aux
+rm ./chapter/Schlussbetrachtung.aux
+rm ./chapter/Sperrvermerk.aux
+
+clear
